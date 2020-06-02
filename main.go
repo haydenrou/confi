d1f36e46fedce471d861194f997419132d48ca11
Hayden Rouille <hayden@rouille.dev>
2020-06-02 17:50:50 -0400

    add show all config

--- main.go
+++ main.go
@@ -1,35 +1,61 @@
 package main
 
-import(
-    "os"
-    "io/ioutil"
+import (
     "fmt"
+    "io/ioutil"
+    "os"
 )
 
+const CONFIG_PATH = "/.ssh/config"
+
+var HOME_PATH = os.Getenv("HOME")
+var SSH_CONFIG_PATH = HOME_PATH + CONFIG_PATH
+
 func main() {
-    var HOME_PATH string = os.Getenv("HOME")
-    var CONFIG_PATH string = "/.ssh/config"
-    var SSH_CONFIG_PATH = HOME_PATH + CONFIG_PATH
+    _, err := ioutil.ReadFile(SSH_CONFIG_PATH)
 
-    data, err := ioutil.ReadFile(SSH_CONFIG_PATH)
+    errMessage := checkValidity(err)
 
-
-    if HOME_PATH == "" {
-        fmt.Println("You must set ENV $HOME to your home path")
+    if errMessage != "" {
+        fmt.Println(errMessage)
         return
-    } else if os.IsNotExist(err) {
-        fmt.Println("You must create a config file in $HOME/.ssh/")
-        return
+    } else if len(os.Args) >= 3 {
+        processStatement(os.Args)
     }
-
-    var SSH_CONFIG = string(data)
-
-    fmt.Println(SSH_CONFIG)
-
-    if len(os.Args) == 1 {
-        fmt.Println("Please enter some arguments")
-        return
-    }
-
-    fmt.Printf("Your first argument is: %v\n", os.Args[1])
 }
+
+func sshConfig() string {
+    data, _ := ioutil.ReadFile(SSH_CONFIG_PATH)
+
+    return string(data)
+}
+
+func checkValidity(err error) string {
+    if HOME_PATH == "" {
+        return "You must set ENV $HOME to your home path"
+    } else if os.IsNotExist(err) {
+        return "You must create a config file in $HOME/.ssh/"
+    } else if len(os.Args) < 3 {
+        return "You must enter a valid statement"
+    } else {
+        return ""
+    }
+}
+
+func processStatement(args []string) {
+    option := args[1]
+    argument := args[2]
+
+    if option == "show" {
+        processShow(argument)
+        return
+    }
+}
+
+func processShow(argument string) {
+    if argument == "all" {
+        fmt.Println(sshConfig())
+        return
+    }
+}
+
