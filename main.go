2ba84d209a65c0c8a95698f92a9601fc07b0b113
Hayden Rouille <hayden@rouille.dev>
2020-06-14 18:29:43 -0400

    Add show

--- main.go
+++ main.go
@@ -4,9 +4,12 @@ import (
     "fmt"
     "io/ioutil"
     "os"
+    "strings"
 )
 
-const CONFIG_PATH = "/.ssh/config"
+const (
+    CONFIG_PATH = "/.ssh/config"
+)
 
 var HOME_PATH = os.Getenv("HOME")
 var SSH_CONFIG_PATH = HOME_PATH + CONFIG_PATH
@@ -19,7 +22,7 @@ func main() {
     if errMessage != "" {
         fmt.Println(errMessage)
         return
-    } else if len(os.Args) >= 3 {
+    } else {
         processStatement(os.Args)
     }
 }
@@ -28,6 +31,34 @@ func sshConfig() string {
     data, _ := ioutil.ReadFile(SSH_CONFIG_PATH)
 
     return string(data)
+}
+
+func mappedSsh() map[string]map[string]string {
+    splitString := strings.Split(sshConfig(), "Host ")
+
+    splitConfigs := make([][]string, len(splitString), len(splitString))
+
+    sshMap := map[string]map[string]string{}
+
+    for i := 0; i < len(splitString); i++ {
+        splitConfigs[i] = strings.Split(splitString[i], "\n")
+    }
+
+    for i := 0; i < len(splitConfigs); i++ {
+        valueMap := map[string]string{}
+
+        for x := 0; x < len(splitConfigs[i]); x++ {
+            vals := strings.Split(strings.TrimSpace(string(splitConfigs[i][x])), " ")
+
+            if len(vals) > 1 {
+                valueMap[vals[0]] = vals[1]
+            }
+        }
+
+        sshMap[strings.TrimSpace(splitConfigs[i][0])] = valueMap
+    }
+
+    return sshMap
 }
 
 func checkValidity(err error) string {
@@ -56,6 +87,14 @@ func processShow(argument string) {
     if argument == "all" {
         fmt.Println(sshConfig())
         return
+    }
+
+    if v, ok := mappedSsh()[argument]; ok {
+        fmt.Printf("Host\t%v\n", argument)
+
+        for k, v := range v {
+            fmt.Printf("\t%v\t %v\n", k, v)
+        }
     }
 }
 
