7136304ed87c2321a936cc4a55a44030966763b8
Hayden Rouille <hayden@rouille.dev>
2020-06-18 21:14:30 -0400

    Remove newline at beginning of written config file

--- writer.go
+++ writer.go
@@ -15,12 +15,26 @@ func WriteConfig(config map[string]map[string]string) {
 
 	writer := bufio.NewWriter(file)
 
+	// We don't want to have a newline at the top of the file...
+	// this isn't great - but it does the job!
+	var count int
+
 	for key, value := range config {
 		if key == "" {
 			continue
 		}
 
-		_, err := writer.WriteString("\nHost " + key + "\n")
+		var hostString string
+
+		switch count {
+		case 0:
+			hostString = "Host "
+		default:
+			hostString = "\nHost "
+		}
+		count++
+
+		_, err := writer.WriteString(hostString + key + "\n")
 
 		if err != nil {
 			log.Fatalf("Error whilst writing to file: %s", err.Error())
